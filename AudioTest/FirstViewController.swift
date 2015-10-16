6aadf2a52dcaa13423bd9b33672608a866b949c3
Christopher McMahon <melophonic@gmail.com>
2015-10-16 12:46:16 -0700

    Initial Commit

---
+++ AudioTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AudioTest
+//
+//  Created by Christopher McMahon on 10/16/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
